965bf35fcd49b33813a0b8e2a42253123472c16f
bboy.stats <bboy.stats@gmail.com>
2018-04-18 12:30:54 +0200

    video list in activities package

--- youtube.go
+++ youtube.go
@@ -2,31 +2,20 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/youtube/activities"
 
 	"google.golang.org/api/googleapi/transport"
 	"google.golang.org/api/youtube/v3"
 )
 
-var (
-	channelId  = flag.String("channel-id", "UCLRCoLpDImkbFw8D3qXFpUQ", "Channel Id exemple")
-	maxResults = flag.Int64("max-results", 25, "Max YouTube results")
-)
+var channelID = flag.String("channel-id", "UCLRCoLpDImkbFw8D3qXFpUQ", "Channel Id exemple")
 
 type Configuration struct {
 	developerKey string
-}
-
-type video struct {
-	title       string
-	description string
-}
-
-type MyService struct {
-	*youtube.Service
 }
 
 func main() {
@@ -39,36 +28,8 @@ func main() {
 		log.Fatalf("Error creating new YouTube client: %v", err)
 	}
 
-	s := &MyService{service}
+	s := &activities.MyService{service}
 
-	videos := s.VideoList(*channelId)
-	printIDs("Videos", videos)
-}
-
-func (service MyService) VideoList(channelId string) map[string]video {
-	flag.Parse()
-
-	call := service.Activities.List("id,snippet").
-		ChannelId(channelId).
-		MaxResults(*maxResults)
-	response, err := call.Do()
-	if err != nil {
-		log.Fatalf("Error making search API call: %v", err)
-	}
-
-	videos := make(map[string]video)
-
-	for _, item := range response.Items {
-		videos[item.Id] = video{title: item.Snippet.Title, description: item.Snippet.Description}
-	}
-
-	return videos
-}
-
-func printIDs(sectionName string, matches map[string]video) {
-	fmt.Printf("%v:\n", sectionName)
-	for id, video := range matches {
-		fmt.Printf("[%v] %v\n", id, video.title)
-	}
-	fmt.Printf("\n\n")
+	videos := s.VideoList(*channelID)
+	activities.PrintIDs("Videos", videos)
 }
